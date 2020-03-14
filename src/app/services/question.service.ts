3cb32d0a24d478a698b5a42ddb66516ed82edafa
Gaurav Ghongde <7gauravghongde@gmail.com>
2020-03-14 15:14:49 +0530

    [patch] added services and changed angular json

---
+++ src/app/services/question.service.ts
@@ -0,0 +1,60 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+
+// const httpOptions = {
+//   headers: new HttpHeaders({
+//     'Content-Type':  'application/json',
+//     'Authorization': `Bearer ${sessionStorage.getItem('currentUser.jwt')}`
+//   })
+// };
+
+//JWT INTERCEPTOR HANDLES IT
+
+@Injectable({
+  providedIn: 'root'
+})
+export class QuestionService {
+
+  // private handleError: HandleError;
+  postQuestionUrl: string = `${environment.apiUrl}/questions/post`;
+  searchQuestionUrl: string = `${environment.apiUrl}/search`
+  getQuestionUrl: string = "";
+
+  constructor(private http: HttpClient) { }
+
+  postQuestion(questionReq: any): Observable<any> {
+    return this.http.post(this.postQuestionUrl, questionReq).pipe();
+  }
+
+  searchQuestion(searchQuery: string) {
+    return this.http.get(this.searchQuestionUrl, {
+      params: {
+        query: searchQuery
+      },
+      observe: 'response'
+    })
+  }
+
+  errorHandler(error: HttpErrorResponse) {
+    return throwError(error.message || "server error.");
+  }
+
+  getQuestion() {
+    return this.http.get(this.getQuestionUrl);
+  }
+
+  // addHero (hero: Hero): Observable<Hero> {
+  //   return this.http.post<Hero>(this.heroesUrl, hero, httpOptions)
+  //     .pipe(
+  //       catchError(this.handleError('addHero', hero))
+  //     );
+  // }
+
+  // searchQuestion(questionTitle: string, questionBody: string): Observable<any>{
+  //   return this.http.post<any>({this.questionTitle, this.questionBody},)
+  // }
+
+}
